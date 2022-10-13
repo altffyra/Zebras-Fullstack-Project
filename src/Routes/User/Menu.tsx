3b0f5b1921defd0d696a26f357c2bcaf7f9767b4
RickyJohansson <Rickjoh91@hotmail.com>
2022-10-13 08:58:12 +0200

    added structure

--- src/Routes/User/Menu.tsx
+++ src/Routes/User/Menu.tsx
@@ -1,4 +1,5 @@
 import './Menu.scss';
+import menu from '../../assets/menu.svg';
 
 type Props = {}
 
@@ -6,51 +7,61 @@ const Menu = (props: Props) => {
   return (
     <div className="menu-wrapper">
 
-      <section className="menu-header--text">
-        <h1>MENY</h1>
-      </section>
-
-      <section className="menu-header-category">
-        <p>KÖTT</p>
-        <p>FISK</p>
-        <p>VEGETARISKT</p>
-      </section>
-
-      <figure className="menu-appetizers-container">
-        <img src="" alt="" />
-        <section className="menu-appetizers-text">
-          <p>FÖRRÄTTER</p>
+      <section className="menu-header--container">
+        <figure className="menu-header--info">
+          <img src={menu} alt="" />
+          <section className="menu-header--text">
+            <section className="menu-header--flex">
+              <h1>MENY</h1>
+            </section>
+          </section>
+        </figure>
+        <section className="menu-header--category">
+          <p>KÖTT</p>
+          <p>FISK</p>
+          <p>VEGETARISKT</p>
         </section>
-      </figure>
+      </section>
 
-      <section className="menu-mainmeals-wrapper">
+      <section className="menu-header--container">
+        <figure className="menu-header--info">
+          <img src={menu} alt="" />
+          <section className="menu-header--text">
+            <section className="menu-header--flex">
+              <p>FÖRRÄTTER</p>
+            </section>
+          </section>
+        </figure>
+      </section>
 
-        <figure className="menu-mainmeals-container">
+      <section className="menu-mainmeals--wrapper">
+
+        <figure className="menu-mainmeals--container">
           <img src="" alt="" />
-          <section className="menu-mainmeal-text">
+          <section className="menu-mainmeal--text">
             <p>HUVUDRÄTTER</p>
           </section>
         </figure>
-        <section className="menu-mainmeal-meat">
+        <section className="menu-mainmeal--meat">
           <p>KÖTT</p>
         </section>
         <section> HÄR LOOPAS KÖTT </section>
 
-        <section className="menu-mainmeal-fish">
+        <section className="menu-mainmeal--fish">
           <p>FISK</p>
         </section>
         <section> HÄR LOOPAS FISK </section>
 
-        <section className="menu-mainmeal-veg">
+        <section className="menu-mainmeal--veg">
           <p>VEGETARISKT</p>
         </section>
         <section> HÄR LOOPAS VEGETARISKT </section>
 
       </section>
 
-      <figure className="menu-desserts-container">
+      <figure className="menu-desserts--container">
         <img src="" alt="" />
-        <section className="menu-dessert-text">
+        <section className="menu-dessert--text">
           <p>EFTERRÄTTER</p>
         </section>
       </figure>
