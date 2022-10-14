1dc78c380a901871f0a085fe0ded8af23fb7a81e
Felix Stark <felix.b.stark@gmail.com>
2022-10-14 12:52:04 +0200

    form structureand functions done

--- src/Routes/User/SignUp.tsx
+++ src/Routes/User/SignUp.tsx
@@ -1,10 +1,59 @@
 import React from 'react'
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { User } from '../../models/Interface';
 import '../../styles/signUp.scss'
-
+import { v4 as uuid } from 'uuid';
 
 type Props = {}
 
 const SignUp = (props: Props) => {
+
+  const [userName, setUserName] = useState<string>('');
+  const [userEmail, setUserEmail] = useState<string>('');
+  const [userPhone, setUserPhone] = useState<string>('');
+  const [userPassword, setUserPassword] = useState<string>('');
+
+
+  const handleSubmit: (e: FormEvent) => void = (e) => {
+    e.preventDefault();
+    addUser();
+  }
+
+  async function addUser() {
+    const newUser: User = {
+      name: userName,
+      email: userEmail,
+      accountId: uuid(),
+      phoneNumber: userPhone,
+      admin: false
+    }
+    const response = await fetch('http://localhost:/user/signup', {
+      method: 'POST',
+      body: JSON.stringify(newUser),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const data = await response.json();
+    console.log(data)
+  }
+
+  
+
+  const handleName: (e: ChangeEvent<HTMLInputElement>) => void = (e) => {
+      setUserName(e.target.value);
+  };
+
+  const handleEmail: (e: ChangeEvent<HTMLInputElement>) => void = (e) => {
+      setUserEmail(e.target.value);
+  };
+
+  const handlePassword: (e: ChangeEvent<HTMLInputElement>) => void = (e) => {
+      setUserPassword(e.target.value);
+  };
+
+  const handlePhone: (e: ChangeEvent<HTMLInputElement>) => void = (e) => {
+      setUserPhone(e.target.value);
+  };
+
 
 
   return (
@@ -14,11 +63,11 @@ const SignUp = (props: Props) => {
         <figure></figure>
         <form>
           <label htmlFor="username">Användarnamn</label>
-          <input type="text" name='username' />
+          <input type="text" name='username' onChange={(e)=> {handleEmail(e)}} />
           <label htmlFor="password">Lösenord</label>
-          <input type="password" name="password" />
+          <input type="password" name="password" onChange={(e)=>{handlePassword(e)}} />
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" onChange={(e)=>{handleEmail(e)}} />
           <button type="submit" className='bigBtn'>Skapa konto</button>
         </form>
       </section>
@@ -27,3 +76,4 @@ const SignUp = (props: Props) => {
 }
 
 export default SignUp
+
