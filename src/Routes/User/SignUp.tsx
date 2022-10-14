bf9af869a1d83e29fa5753a1b7b05b4de3a9fed9
Felix Stark <felix.b.stark@gmail.com>
2022-10-14 09:42:20 +0200

    signup form

--- src/Routes/User/SignUp.tsx
+++ src/Routes/User/SignUp.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import '../../styles/signUp.scss'
+
 
 type Props = {}
 
 const SignUp = (props: Props) => {
+
+
   return (
-    <div>SignUp</div>
+    <div className='signUp'>
+      <button className='smallBtn'>Tillbaka</button>
+      <section className="flex-container">
+        <figure></figure>
+        <form>
+          <label htmlFor="username">Användarnamn</label>
+          <input type="text" name='username' />
+          <label htmlFor="password">Lösenord</label>
+          <input type="password" name="password" />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" />
+          <button type="submit" className='bigBtn'>Skapa konto</button>
+        </form>
+      </section>
+    </div>
   )
 }
 
