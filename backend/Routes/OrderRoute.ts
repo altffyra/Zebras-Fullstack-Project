76fc85b73e9b74a2d3fc8f73134c1c24f1057c98
Christian Johnsson <chjohnsson@hotmail.com>
2022-10-14 14:00:50 +0200

    admin order return and params orders

--- backend/Routes/OrderRoute.ts
+++ backend/Routes/OrderRoute.ts
@@ -2,17 +2,36 @@ import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json());
 const orderRoute = express.Router();
+import {getOrders} from '../lowDb/database.js'
+import {Order} from '../lowDb/dbinterface.js'
+
+
+
 
 // GET USER ORDERS
+type IdObject = { id: string };
+type IdParam = Request<IdObject>;
+
+orderRoute.get("/user/:id", async (req:IdParam, res:Response) => {
+    const id:string = req.params.id;
+    let resOrders = await getOrders()
+    let filter = resOrders.orders.filter((order:Order) => order.id == id);
+
+  if (filter.length > 0) {
+    res.send(filter);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 
-
-orderRoute.get("/", async (req:Request, res:Response) => {
-    
-
-    res.json('getOrders!')
+// GET ALL ORDERS ADMIN
+orderRoute.get("/admin", async (req:Request, res:Response) => {
+    const resOrders:Order[] = await getOrders()
+    res.json(resOrders)
 })
-// GET ORDERS
+
+
 
 // GET ORDER
 
