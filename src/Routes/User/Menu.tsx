9d0c44e8da99539f4907d90581d4ad03656f74b3
RickyJohansson <Rickjoh91@hotmail.com>
2022-10-14 14:56:52 +0200

    basic frontend structure completed

--- src/Routes/User/Menu.tsx
+++ src/Routes/User/Menu.tsx
@@ -1,72 +1,78 @@
 import './Menu.scss';
-import menu from '../../assets/menu.svg';
-import mainmeal from '../../assets/mainmeal.svg';
+import appertizer from '../../assets/menu/appertizer.svg';
+import mainmeal from '../../assets/menu/mainmeal.svg';
+import veg from '../../assets/menu/veg.svg';
+import dessert from '../../assets/menu/dessert.svg';
+import vector from '../../assets/menu/vector.svg';
+import allergy from '../../assets/menu/allergy.svg';
+import MenuTopic from '../../components/MenuTopic';
+import MenuItem from '../../components/MenuItem';
 
 type Props = {}
 
 const Menu = (props: Props) => {
+
+  const appertizerTopic = "FÖRRÄTT";
+  const appertizerImg = appertizer;
+  const vegTopic = "VEGETARISKT";
+  const vegImg = veg;
+  const dessTopic = "DESSERT";
+  const dessImg = dessert;
+
   return (
     <div className="menu-wrapper">
 
       <section className="menu-header--container">
+
         <figure className="menu-header--info">
-          <img src={menu} alt="" />
+          <img src={mainmeal} alt="" />
           <section className="menu-header--text">
             <section className="menu-header--flex">
               <h1>MENY</h1>
             </section>
           </section>
         </figure>
+
         <section className="menu-header--category">
-          <p>KÖTT</p>
-          <p>FISK</p>
-          <p>VEGETARISKT</p>
+          <h2>KÖTT</h2>
+          <h2>FISK</h2>
+          <h2>FÅGEL</h2>
         </section>
       </section>
 
-      <section className="menu-header--container">
-        <figure className="menu-header--info">
-          <img src={menu} alt="" />
-          <section className="menu-header--text">
-            <section className="menu-header--flex">
-              <p>FÖRRÄTTER</p>
-            </section>
-          </section>
-        </figure>
-      </section>
+      < MenuTopic topic={appertizerTopic} foodImg={appertizerImg}/>
 
       <section className="menu-header--container">
         <figure className="menu-header--info">
           <img src={mainmeal} alt="" />
           <section className="menu-header--text">
             <section className="menu-header--flex">
-              <p>HUVUDRÄTTER</p>
+              <h1>HUVUDRÄTTER</h1>
             </section>
           </section>
         </figure>
-        <section className="menu-title--text">
-          <p className="menu-title--margin">KÖTT</p>
-        </section>
-        <section> HÄR LOOPAS KÖTT </section>
 
         <section className="menu-title--text">
-          <p className="menu-title--margin"> FISK</p>
+          <h2 className="menu-title--margin">KÖTT</h2>
         </section>
-        <section> HÄR LOOPAS FISK </section>
+        <MenuItem vector={vector} allergy={ allergy } foodTopic={ "kött-namn" }/>
 
         <section className="menu-title--text">
-          <p className="menu-title--margin">VEGETARISKT</p>
+          <h2 className="menu-title--margin"> FISK</h2>
         </section>
-        <section> HÄR LOOPAS VEGETARISKT </section>
+        <MenuItem vector={vector} allergy={ allergy } foodTopic={ "fisk-namn" }/>
+        <MenuItem vector={vector} allergy={ allergy } foodTopic={ "fisk-namn" }/>
 
+
+        <section className="menu-title--text">
+          <h2 className="menu-title--margin">FÅGEL</h2>
+        </section>
+        <MenuItem vector={vector} allergy={ allergy } foodTopic={ "fågel-namn" }/>
       </section>
 
-      <figure className="menu-desserts--container">
-        <img src="" alt="" />
-        <section className="menu-dessert--text">
-          <p className="menu-title-margin">EFTERRÄTTER</p>
-        </section>
-      </figure>
+      < MenuTopic topic={vegTopic} foodImg={vegImg}/>
+      < MenuTopic topic={dessTopic} foodImg={dessImg}/>
+
 
     </div>
   )
