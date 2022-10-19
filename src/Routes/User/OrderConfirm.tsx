a9fb78defef93d86ce2b07aa9c2f039a605a68a7
timand1 <80768929+timand1@users.noreply.github.com>
2022-10-19 08:59:37 +0200

    Initiallize order confirm

--- src/Routes/User/OrderConfirm.tsx
+++ src/Routes/User/OrderConfirm.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { Order } from '../../models/Interface';
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../store';
 
-type Props = {}
+const OrderConfirm = () => {
+  const [loading, setLoading] = useState<boolean>(false);
 
-const OrderConfirm = (props: Props) => {
+  const confirmedOrder: Order = useSelector((state: RootState) => state.orders);
+
   return (
-    <div>OrderConfirm</div>
+    <section className="confirmed">
+        {loading ? 
+            <div className='loading'></div>
+            : ''
+        }
+    
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8131.132788853451!2d13.520668944233146!3d59.369957206767886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465cb19d87d4c3c7%3A0x300ad4aa5764fa28!2sLambergskajen%2C%20652%2021%20Karlstad!5e0!3m2!1ssv!2sse!4v1665738091678!5m2!1ssv!2sse" loading="lazy" ></iframe>
+    </section>
   )
 }
 
