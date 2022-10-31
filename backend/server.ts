00292e50b2a75650354ccdf831f7d703a87d2f93
timand1 <80768929+timand1@users.noreply.github.com>
2022-10-31 11:52:45 +0100

    fix port for render.com

--- backend/server.ts
+++ backend/server.ts
@@ -8,7 +8,7 @@ import menuRoute from './Routes/MenuRoute.js'
 import cors from 'cors';
 
 const app = express();
-const PORT: number = 8000;
+const PORT: number = 8080;
 
 app.use(cors({ origin: "*" }));
 
@@ -17,19 +17,9 @@ app.use(express.json());
 const __dirname:string = dirname(fileURLToPath(import.meta.url));
 const staticPath:string = join(__dirname, '../../dist')
 
-
-
 app.use("/api/menu", menuRoute)
 app.use("/api/order", orderRoute)
 app.use("/api/user", userRoute)
-
-// HA KVAR FÖR DET AKTIVERAR DATABASEN
-// @ts-ignore
-
-// async function start(){
-// // const lolw = await lol()
-// }
-// start()
 
 app.use(express.static(staticPath))
 
@@ -37,21 +27,3 @@ app.use(express.static(staticPath))
 app.listen(PORT, () => {
     console.log("Running on ", PORT);
   });
-  
-
-//cartItems: MenuItems[];
-////totalPrice: number;
-//user: User;
-////userComment: string;
-//adminComment: string;
-//locked: boolean;
-//completed: boolean;
-//orderPlaced: string;
-
-
-
-//name: string;
-//email: string;
-//accountId: string;
-//phoneNumber: string;
-//admin?: boolean;
