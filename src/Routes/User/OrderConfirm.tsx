844a69a09987d18a6b314d0654a017793fc10916
Christian Johnsson <chjohnsson@hotmail.com>
2022-11-08 15:10:37 +0100

    rätt karta

--- src/Routes/User/OrderConfirm.tsx
+++ src/Routes/User/OrderConfirm.tsx
@@ -157,10 +157,7 @@ const OrderConfirm = () => {
           <p className="open-text">Vi har öppet dygnet runt!</p>
         </div>
       </div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8131.132788853451!2d13.520668944233146!3d59.369957206767886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465cb19d87d4c3c7%3A0x300ad4aa5764fa28!2sLambergskajen%2C%20652%2021%20Karlstad!5e0!3m2!1ssv!2sse!4v1665738091678!5m2!1ssv!2sse"
-        loading="lazy"
-      ></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2032.774306644913!2d13.527552816657577!3d59.37010558167321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465cb17627d78491%3A0xe07a4122ee00218d!2sLambergskajen%2056%2C%20652%2021%20Karlstad!5e0!3m2!1ssv!2sse!4v1667916416392!5m2!1ssv!2sse" loading="lazy"></iframe>
       {deleteOverlay ? (
         <div className="delete-overlay">
           <div className="delete-container">
